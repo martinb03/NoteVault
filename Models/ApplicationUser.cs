bc69b96aa701b611667c7f5080d7cd49b996d1d9
martinb03 <martoln7@gmail.com>
2026-05-16 17:28:17 +0300

    Add Navigation properties to User Model

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -5,4 +5,9 @@ public class ApplicationUser : IdentityUser
 {
     public string DisplayName { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
+    // Navigation
+    public ICollection<Folder> Folders { get; set; } = new List<Folder>();
+    public ICollection<Note> Notes { get; set; } = new List<Note>();
+    public ICollection<Tag> Tags { get; set; } = new List<Tag>();
 }
